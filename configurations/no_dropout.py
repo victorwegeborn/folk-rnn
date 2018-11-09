c783025f024a7d844f7cc48b7c6f05fed6433167
victorhv <victorhv@kth.se>
2018-11-09 12:52:29 +0100

    Final experiment configs

---
+++ configurations/no_dropout.py
@@ -0,0 +1,18 @@
+one_hot = True
+embedding_size = 256  # is ignored if one_hot=True
+num_layers = 3
+rnn_size = 800 # ~4 x |V|
+dropout = 0
+
+learning_rate = 0.003
+learning_rate_decay_after = 20
+learning_rate_decay = 0.97
+
+batch_size = 32
+max_epoch = 90 # 60 > 30
+grad_clipping = 5
+validation_fraction = 0.05
+validate_every = 1000  # iterations
+
+save_every = 5  # epochs
+resume_path='...'
